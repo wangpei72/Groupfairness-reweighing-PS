103222ea55ca8e610bd82afd02cc318c731e8ca6
pei Wang <44791528+wangpei72@users.noreply.github.com>
2022-04-10 15:18:25 +0800

    first commit

---
+++ load_model/model_retrain.py
@@ -0,0 +1,74 @@
+import numpy as np
+import sys
+sys.path.append("../")
+
+import tensorflow as tf
+from tensorflow.python.platform import flags
+
+from utils.utils_tf import *
+from tutorial_models import dnn
+
+FLAGS = flags.FLAGS
+
+def training(dataset, model_path, nb_epochs, batch_size,learning_rate):
+    """
+    Train the model
+    :param dataset: the name of testing dataset
+    :param model_path: the path to save trained model
+    """
+
+    # prepare the data and model
+    X=np.load("../data/census/data-x.npy")
+    Y=np.load("../data/census/data-y.npy")
+    input_shape=(None,13)
+    nb_classes = 2
+
+    tf.set_random_seed(1234)
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.8
+    sess = tf.Session(config=config)
+    x = tf.placeholder(tf.float32, shape=input_shape)
+    y = tf.placeholder(tf.float32, shape=(None, nb_classes))
+    model = dnn(input_shape, nb_classes)
+    preds = model(x)
+    saver = tf.train.Saver()
+    #原先的模型路径(即使用原始数据集训练的模型)
+    modelpath = "../my-model/census/999/test.model"
+
+    saver.restore(sess, modelpath)
+
+    # training parameters
+    train_params = {
+        'nb_epochs': nb_epochs,
+        'batch_size': batch_size,
+        'learning_rate': learning_rate,
+        'train_dir': model_path + dataset + "/",
+        'filename': 'test.model'
+    }
+
+    # training procedure
+    sess.run(tf.global_variables_initializer())
+    rng = np.random.RandomState([2019, 7, 15])
+    model_train(sess, x, y, preds, X, Y, args=train_params,
+                rng=rng, save=True)
+
+    # evaluate the accuracy of trained model
+    eval_params = {'batch_size': 128}
+    accuracy = model_eval(sess, x, y, preds, X, Y, args=eval_params)
+    print('Test accuracy on legitimate test examples: {0}'.format(accuracy))
+
+def main(argv=None):
+    training(dataset = FLAGS.dataset,
+             model_path = FLAGS.model_path,
+             nb_epochs=FLAGS.nb_epochs,
+             batch_size=FLAGS.batch_size,
+             learning_rate=FLAGS.learning_rate)
+
+if __name__ == '__main__':
+    flags.DEFINE_string("dataset", "census", "the name of dataset")
+    flags.DEFINE_string("model_path", "../re-model/", "the name of path for saving model")
+    flags.DEFINE_integer('nb_epochs', 100, 'Number of epochs to train model')
+    flags.DEFINE_integer('batch_size', 128, 'Size of training batches')
+    flags.DEFINE_float('learning_rate', 0.01, 'Learning rate for training')
+
+    tf.app.run()
