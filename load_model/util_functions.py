3b96a394a617cd953d92d6dbf6d2a9d0389d7894
pei Wang <44791528+wangpei72@users.noreply.github.com>
2022-04-13 16:45:00 +0800

    ready to test on host

---
+++ load_model/util_functions.py
@@ -0,0 +1,108 @@
+import sys
+
+sys.path.append("../")
+import numpy as np
+from load_model.network import *
+from load_model.layer import *
+
+def dnn(input_shape=(None, 13), nb_classes=2):
+    """
+    The implementation of a DNN model
+    :param input_shape: the shape of dataset
+    :param nb_classes: the number of classes
+    :return: a DNN model
+    """
+    activation = ReLU
+    layers = [Linear(64),
+              activation(),
+              Linear(32),
+              activation(),
+              Linear(16),
+              activation(),
+              Linear(8),
+              activation(),
+              Linear(4),
+              activation(),
+              Linear(nb_classes),
+              Softmax()]
+
+    model = MLP(layers, input_shape)
+    return model
+
+
+def gradient_graph(x, preds, y=None):
+    """
+    Construct the TF graph of gradient
+    :param x: the input placeholder
+    :param preds: the model's symbolic output
+    :return: the gradient graph
+    """
+    if y == None:
+        # Using model predictions as ground truth to avoid label leaking
+        preds_max = tf.reduce_max(preds, 1, keep_dims=True)
+        y = tf.to_float(tf.equal(preds, preds_max))
+        y = tf.stop_gradient(y)
+    y = y / tf.reduce_sum(y, 1, keep_dims=True)
+
+    # Compute loss
+    loss = model_loss(y, preds, mean=False)
+
+    # Define gradient of loss wrt input
+    grad, = tf.gradients(loss, x)
+
+    return grad
+
+
+def model_loss(y, model, mean=True):
+    """
+    Define loss of TF graph
+    :param y: correct labels
+    :param model: output of the model
+    :param mean: boolean indicating whether should return mean of loss
+                 or vector of losses for each input of the batch
+    :return: return mean of loss if True, otherwise return vector with per
+             sample loss
+    """
+
+    op = model.op
+    if op.type == "Softmax":
+        logits, = op.inputs
+    else:
+        logits = model
+
+    out = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y)
+
+    if mean:
+        out = tf.reduce_mean(out)
+    return out
+
+
+def model_argmax(sess, x, predictions, samples, feed=None):
+    """
+    Helper function that computes the current class prediction
+    :param sess: TF session
+    :param x: the input placeholder
+    :param predictions: the model's symbolic output
+    :param samples: numpy array with input samples (dims must match x)
+    :param feed: An optional dictionary that is appended to the feeding
+             dictionary before the session runs. Can be used to feed
+             the learning phase of a Keras model for instance
+    :return: the argmax output of predictions, i.e. the current predicted class
+    """
+    feed_dict = {x: samples}
+    if feed is not None:
+        feed_dict.update(feed)
+    probabilities = sess.run(predictions, feed_dict)
+
+    if samples.shape[0] == 1:
+        return np.argmax(probabilities)
+    else:
+        return np.argmax(probabilities, axis=1)
+
+
+def model_probab(sess, x, predictions, samples, feed=None):
+    feed_dict = {x: samples}
+    if feed is not None:
+        feed_dict.update(feed)
+    probabilities = sess.run(predictions, feed_dict)
+    return probabilities
---
+++ reweighing_resampling/ranker_learner.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append("../")
+import numpy as np
+import common_utils as cu
+from load_model.util_functions import *
+
+input_shape = (None, 13)
+nb_classes = 2
+tf.set_random_seed(1234)
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
+sess = tf.Session(config=config)
+x = tf.placeholder(tf.float32, shape=input_shape)
+y = tf.placeholder(tf.float32, shape=(None, nb_classes))
+model = dnn(input_shape, nb_classes)
+preds = model(x)
+saver = tf.train.Saver()
+model_path = '../my-model/census/999/' + 'test.model'
+saver.restore(sess, model_path)
+
+# construct the gradient graph
+grad_0 = gradient_graph(x, preds)
+# predict
+if __name__ == '__main__':
+    x_origin = np.load('../data/census/data-x.npy')
+    y_origin = np.load('../data/census/data-y.npy')
+    ranker_array = np.ndarray((x_origin.shape[0], 2), dtype=np.float32)
+    for i in range(x_origin.shape[0]):
+        label_tmp = model_argmax(sess, x, preds, np.array([x_origin[i]]))
+        ranker_score = model_probab(sess, x, preds, np.array([x_origin[i]]))
+        ranker_array[i] = ranker_score
+    np.save('ranker_result_origin/2dims_result.npy', ranker_array)
+
